4ff6cd05e5ff371f1487025697c114afd4de646e
AirToggle <airtoggle@gmail.com>
2024-06-03 16:06:38 +0200

    1. uvodnáobr_uz_spravne

--- app/src/main/java/com/example/semestralka_quiz_geo/MainActivity.kt
+++ app/src/main/java/com/example/semestralka_quiz_geo/MainActivity.kt
@@ -1,25 +1,80 @@
 package com.example.semestralka_quiz_geo
 
-import android.content.Intent
+import androidx.compose.ui.Modifier
 import android.os.Bundle
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState : Bundle?) {
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main) // ktoré ekno sa pouzíva
-        val buttonSpusti: Button = findViewById(R.id.button) // spojazdnenie buttonu
-        val buttonZavri: Button = findViewById(R.id.button2)
+        setContent {
+            Surface {
+                obrazovka()
+            }
 
-        buttonSpusti.setOnClickListener {
-            val intent = Intent(this, hl_menu::class.java)
-            startActivity(intent)
-        }
-
-        buttonZavri.setOnClickListener {
-            finishAffinity()
         }
     }
 }
 
+@Composable
+fun obrazovka() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = painterResource(id = R.drawable.uvod),
+            contentDescription = "pozadie",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.matchParentSize()
+        )
+    }
+    Column(
+        modifier = Modifier.fillMaxSize()
+        .padding(top = 64.dp),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Vitaj", fontSize = 32.sp,)
+        Button(
+            onClick = { },
+            modifier = Modifier.padding(bottom = 48.dp),
+            colors = ButtonDefaults.buttonColors(Color.Green)
+        ) {
+            Text(text = "Klikni sem pre pokračovanie", fontSize = 16.sp)
+        }
+    }
+}
+
+    @Preview(showBackground = true)
+    @Composable
+    fun obrazovkaPreview() {
+        obrazovka()
+    }
